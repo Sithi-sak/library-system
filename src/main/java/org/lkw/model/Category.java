f37dd282598db1cc5c16be8a0ca3dcde33870b03
Leak Sithisak <sithisakleak001@gmail.com>
2025-04-06 17:04:05 +0700

    add Category

---
+++ src/main/java/org/lkw/model/Category.java
@@ -0,0 +1,45 @@
+package org.lkw.model;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+    private String description;
+
+    // Getters and Setters
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return categoryName;
+    }
+
+    public void setName(String name) {
+        this.categoryName = name;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return categoryName;
+    }
+} 
