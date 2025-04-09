e17b747bd246988da2790a2c2eba59bea91d67c6
Leak Sithisak <sithisakleak001@gmail.com>
2025-04-10 00:03:32 +0700

    handle user session

---
+++ src/main/java/org/lkw/model/UserSession.java
@@ -0,0 +1,27 @@
+package org.lkw.model;
+
+public class UserSession {
+    private static UserSession instance;
+    private User currentUser;
+
+    private UserSession() {}
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void clearSession() {
+        currentUser = null;
+    }
+} 
